10ad0c971df33bd76086e0b234e94ea3d12d109e
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2022-03-07 11:24:13 -0500

    projects component, added project cards with links, adding more folders for organization, created css files

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import Home from './components/Home';
+import Home from './components/Home/Home.jsx';
+import NavBar from './components/UI/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Home />
     </div>
   );
