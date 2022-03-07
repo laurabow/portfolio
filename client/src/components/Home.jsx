ae20a4a7dfdc0cb393892976ab1e110ea490278d
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2022-03-07 10:58:13 -0500

    started homepage

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h3>Hi, I'm Laura</h3>
+      <p><span>I am a fullstack software engineer, musician, former bakery owner, and a dog mom. I am a compassionate and hard working person who brings passion to every project. I approach problems with empathy, creativity, and a drive to overcome any obstacle. With enthusiasm and passion, I work hard and always strive to learn new languages and methods. I bring a positive attitude to any workplace. I am reliable, goal-driven, and show grace under pressure.</span><br/><span>Email me at: laurabowman1024@gmail.com</span></p>
+    </div>
+  )
+}
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+
+export default function NavBar() {
+  return (
+    <div>
+      <h1>Laura Bowman</h1>
+      
+    </div>
+  )
+}
