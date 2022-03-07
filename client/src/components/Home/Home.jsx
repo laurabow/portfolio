933b45136a27e35cb2dcfea4514c425ce22fc707
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2022-03-07 12:30:52 -0500

    homepage styling, updated link positions in project cards, skills styling

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -8,19 +8,27 @@ export default function Home() {
       <h2 className='hi'>Hi, I'm Laura</h2>
       <p className='brand'><span className='brand-statement'>I am a fullstack software engineer, musician, former bakery owner, and a dog mom. I am a compassionate and hard working person who brings passion to every project. I approach problems with empathy, creativity, and a drive to overcome any obstacle. With enthusiasm and passion, I work hard and always strive to learn new languages and methods. I bring a positive attitude to any workplace. I am reliable, goal-driven, and show grace under pressure.</span><br /><span className='email'>Email me at: laurabowman1024@gmail.com</span></p>
       <Projects />
-      <div>
-        <h3>Skills</h3>
-        <h5>Languages: </h5>
-        <p>JavaScript, Ruby, Node.js, HTML, CSS</p>
+      <div className='skills-container'>
+        <h2>Skills</h2>
+        <div className='skills'>
+          <h4>Languages: </h4>
+          <p>JavaScript, Ruby, Node.js, HTML, CSS</p>
+        </div>
         <br />
-        <h5>Frameworks: </h5>
-        <p>React, Ruby on Rails, Express, Node.js</p>
+        <div className='skills'>
+          <h4>Frameworks: </h4>
+          <p>React, Ruby on Rails, Express, Node.js</p>
+        </div>
         <br />
-        <h5>Databases: </h5>
-        <p>PostgreSQL, SQL, RESTful APIs, MongoDB</p>
+        <div className='skills'>
+          <h4>Databases: </h4>
+          <p>PostgreSQL, SQL, RESTful APIs, MongoDB</p>
+        </div>
         <br />
-        <h5>Tools: </h5>
-        <p>GitHub, Git, Heroku</p>
+        <div className='skills'>
+          <h4>Tools: </h4>
+          <p>GitHub, Git, Heroku</p>
+        </div>
       </div>
       <div>
         <h3>Download my resume</h3>
--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -9,44 +9,50 @@ export default function Projects() {
       <div className='card-container'>
         <div className='card'>
           <h4>Party Finder</h4>
-          <a
-            href='https://github.com/laurabow/party-finder'
-            target="_blank"
-            rel='noopener noreferrer'>Github Repo</a>
-          <a
-            href='https://ttrpg-party-finder.netlify.app/'
-            target="_blank"
-            rel='noopener noreferrer'>Deployed Link</a>
+          <div className='link-div'>
+            <a
+              href='https://github.com/laurabow/party-finder'
+              target="_blank"
+              rel='noopener noreferrer'>Github Repo</a>
+            <a
+              href='https://ttrpg-party-finder.netlify.app/'
+              target="_blank"
+              rel='noopener noreferrer'>Deployed Link</a>
+          </div>
           <br/>
           <p>Party Finder is an online platform for table top roleplaying game players to find games to play in and for game masters to find players for their games.</p>
         </div>
         <div className='card'>
           <h4>Caves and Wyverns</h4>
-          <a
-            href='https://github.com/laurabow/P2-Caves-Wyverns'
-            target="_blank"
-            rel='noopener noreferrer'>Github Repo</a>
-          <a
-            href='https://caves-and-wyverns.netlify.app/'
-            target="_blank"
-            rel='noopener noreferrer'>Deployed Link</a>
+          <div className='link-div'>
+            <a
+              href='https://github.com/laurabow/P2-Caves-Wyverns'
+              target="_blank"
+              rel='noopener noreferrer'>Github Repo</a>
+            <a
+              href='https://caves-and-wyverns.netlify.app/'
+              target="_blank"
+              rel='noopener noreferrer'>Deployed Link</a>
+            </div>
           <br/>
           <p>Cave and Wyverns is a dice game made with react. Users roll a dice and if they win, points get taken from the health points of the monster, if they lose the roll they lose health points.</p>
         </div>
         <div className='card'>
           <h4>Volunteer app</h4>
-          <a
-            href='https://github.com/laurabow/VolunteerApp'
-            target="_blank"
-            rel='noopener noreferrer'>Front End Github Repo</a>
-          <a
-            href='https://github.com/laurabow/VolunteerAppBackend'
-            target="_blank"
-            rel='noopener noreferrer'>Back End Github Repo</a>
-          <a
-            href='https://hungry-borg-6b9f04.netlify.app/'
-            target="_blank"
-            rel='noopener noreferrer'>Deployed Link</a>
+          <div className='link-div'>
+            <a
+              href='https://github.com/laurabow/VolunteerApp'
+              target="_blank"
+              rel='noopener noreferrer'>Front End Github Repo</a>
+            <a
+              href='https://github.com/laurabow/VolunteerAppBackend'
+              target="_blank"
+              rel='noopener noreferrer'>Back End Github Repo</a>
+            <a
+              href='https://hungry-borg-6b9f04.netlify.app/'
+              target="_blank"
+              rel='noopener noreferrer'>Deployed Link</a>
+            </div>
           <br/>
           <p>The Volunteer App is an app where non-profit organizations can post events that need volunteers. It is also a place for volunteers to sign up to help out at those events. This was a group project.</p>
         </div>
