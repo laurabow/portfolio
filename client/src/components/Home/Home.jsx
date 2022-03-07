c3ca878fcc97f8d7f3aa0416b2454a59c6730c92
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2022-03-07 12:08:40 -0500

    homepage styling

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -4,9 +4,9 @@ import './Home.css';
 
 export default function Home() {
   return (
-    <div>
-      <h2>Hi, I'm Laura</h2>
-      <p><span>I am a fullstack software engineer, musician, former bakery owner, and a dog mom. I am a compassionate and hard working person who brings passion to every project. I approach problems with empathy, creativity, and a drive to overcome any obstacle. With enthusiasm and passion, I work hard and always strive to learn new languages and methods. I bring a positive attitude to any workplace. I am reliable, goal-driven, and show grace under pressure.</span><br /><span>Email me at: laurabowman1024@gmail.com</span></p>
+    <div className='homepage'>
+      <h2 className='hi'>Hi, I'm Laura</h2>
+      <p className='brand'><span className='brand-statement'>I am a fullstack software engineer, musician, former bakery owner, and a dog mom. I am a compassionate and hard working person who brings passion to every project. I approach problems with empathy, creativity, and a drive to overcome any obstacle. With enthusiasm and passion, I work hard and always strive to learn new languages and methods. I bring a positive attitude to any workplace. I am reliable, goal-driven, and show grace under pressure.</span><br /><span className='email'>Email me at: laurabowman1024@gmail.com</span></p>
       <Projects />
       <div>
         <h3>Skills</h3>
